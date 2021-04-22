256bb756e581fb0685704b8960b2ed6d4b7ede4b
MMARIN1 <mauricio-marino@hotmail.com>
2021-04-21 22:30:28 -0500

    Docs agregado

---
+++ docs/precache-manifest.169911cefb07c5fddce9465215ff4e24.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "427addf8a870d59a81f27f1d88432178",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f42d0d43b093cd58ef34",
+    "url": "/static/css/main.832776f2.chunk.css"
+  },
+  {
+    "revision": "8c639a6bff16b54b9f77",
+    "url": "/static/js/2.629c55af.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.629c55af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f42d0d43b093cd58ef34",
+    "url": "/static/js/main.afb5b778.chunk.js"
+  },
+  {
+    "revision": "5bba4b9ed69c6568a70e",
+    "url": "/static/js/runtime-main.95118175.js"
+  }
+]);
